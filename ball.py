73c769c2e960cf2566f553b208a378a544682881
Daniel Marchi Silva <daniel.silva@userede.com.br>
2019-11-13 19:29:53 -0300

    ball agora se move nos eixos x e y

--- ball.py
+++ ball.py
@@ -6,19 +6,43 @@ class Ball:
     def __init__(self):
         self.rect = pygame.Rect(400, 150, 15, 15)
         self.left = True
+        self.down = True
 
 
     def move(self):
+
+        if(self.down is True):
+            self.move_down()
+        else:
+            self.move_Up()
+
+        
         if(self.left is True):
             self.moveLeft()
         else:
             self.moveRight()
             
 
+    def move_up(self):
+
+        if (self.rect.top > 0 and self.down is False):
+            self.rect.top -=1
+        
+        if(self.rect.top ==0):
+            self.down=True
+
+    
+    def move_down(self):
+        print(self.rect.top)
+        if (self.rect.top < 585 and self.down is True):
+            self.rect.top+=1
+        if (self.rect.top == 585):
+            self.down=False
+
 
     
     def moveLeft(self):
-        print(f'Left{self.rect.left}')        
+        # print(f'Left{self.rect.left}')        
         if (self.rect.left > 5 and self.left is True):
             self.rect.left = self.rect.left-1
 
@@ -26,7 +50,7 @@ class Ball:
             self.left=False
 
     def moveRight(self):        
-        print(f'Right{self.rect.left}')        
+        # print(f'Right{self.rect.left}')        
         if (self.rect.left < 785 and self.left is False):
             self.rect.left = self.rect.left+1
 
